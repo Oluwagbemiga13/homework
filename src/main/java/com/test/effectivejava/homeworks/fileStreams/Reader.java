b6d1f0c2fbda2846c3547c419c28072d8e173b6e
Oluwagbemiga13 <daniel.rakovsky@seznam.cz>
2022-10-14 10:35:48 +0200

    Added Javadoc

--- src/main/java/com/test/effectivejava/homeworks/fileStreams/Reader.java
+++ src/main/java/com/test/effectivejava/homeworks/fileStreams/Reader.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
